fd4be7bbc67c2b265da4e60923eeb3232e9c1ae6
Edenbel27 <151971602+Edenbel27@users.noreply.github.com>
2024-02-29 16:04:55 -0800

    django first project

---
+++ comment/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Comment(models.Model):
+    
+    #content, created time, and modified time.
+    
+    name = models.CharField(max_length=250)
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now=True)
+    modified_time = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
